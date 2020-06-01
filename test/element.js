2c809379bb7e7d5d52b206452823934c1d728f99
Tuan Anh Tran <me@tuananh.org>
2020-06-01 09:45:55 +0700

    test: add startElement test

---
+++ test/element.js
@@ -0,0 +1,19 @@
+const test = require('ava')
+const SaxParser = require('..')
+
+test(`.on('startElement') test`, (t) => {
+    const xml = '<hello>world</hello>'
+    const parser = new SaxParser()
+
+    let eleName
+    parser.on('startElement', (name) => {
+        eleName = name
+    })
+
+    parser.on('endDocument', () => {
+        t.is(eleName, 'hello', 'element name is `hello`')
+        t.pass()
+    })
+
+    parser.parse(xml)
+})
