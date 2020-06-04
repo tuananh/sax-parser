e05a9ac7e2ec55d99875f4a86d8e00fcb4dc97b2
Tuan Anh Tran <me@tuananh.org>
2020-06-04 09:20:41 +0700

    test: add xml decl test

--- test/xml-declaration.js
+++ test/xml-declaration.js
@@ -1,7 +1,7 @@
 const test = require('ava')
 const SaxParser = require('..')
 
-test('XML declaration with encoding', (t) => {
+test('XML declaration with version and encoding', (t) => {
     const xml = "<?xml version='1.0' encoding='UTF-8'?>"
     const parser = new SaxParser()
 
@@ -12,6 +12,25 @@ test('XML declaration with encoding', (t) => {
 
     parser.on('endDocument', () => {
         t.deepEqual(xmlDecls, [{ version: '1.0', encoding: 'UTF-8' }])
+        t.pass()
+    })
+
+    parser.parse(xml)
+})
+
+test('XML declaration with version, encoding and standalone', (t) => {
+    const xml = "<?xml version='1.0' encoding='UTF-8' standalone='no'?>"
+    const parser = new SaxParser()
+
+    let xmlDecls = []
+    parser.on('xmlDecl', (xmlDecl) => {
+        xmlDecls.push(xmlDecl)
+    })
+
+    parser.on('endDocument', () => {
+        t.deepEqual(xmlDecls, [
+            { version: '1.0', encoding: 'UTF-8', standalone: 'no' },
+        ])
         t.pass()
     })
 
