d592a444921384922bec0103b09fa291f958dd1a
Tuan Anh Tran <me@tuananh.org>
2020-06-05 06:22:33 +0700

    test: comment test

--- test/comment.test.js
+++ test/comment.test.js
@@ -1,7 +1,7 @@
 const parse = require('.')
 
 // TODO(anh): figure out why this test is failing
-test.skip('only comment', async () => {
+test('only comment', async () => {
     const xml = `<!--This is example of comment in XML1-->`
     expect(await parse(xml)).toEqual([
         ['comment', 'This is example of comment in XML1'],
@@ -19,15 +19,18 @@ test('single comment inside a node', async () => {
     ])
 })
 
-test('comment test', async () => {
+test('multiple comments test', async () => {
     const xml = `<hello>
     <!--This is example of comment in XML1-->
     <!--This is example of comment in XML2-->
+    <!--This is example of comment in XML3-->
     </hello>`
+
     expect(await parse(xml)).toEqual([
         ['startElement', 'hello', {}],
         ['comment', 'This is example of comment in XML1'],
         ['comment', 'This is example of comment in XML2'],
+        ['comment', 'This is example of comment in XML3'],
         ['endElement', 'hello'],
     ])
 })
