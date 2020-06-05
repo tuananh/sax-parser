9c4dd6a23198d1cb1299f3b08d1248f748b78c6f
Tuan Anh Tran <me@tuananh.org>
2020-06-05 08:43:27 +0700

    test: more test

---
+++ test/other.test.js
@@ -0,0 +1,10 @@
+const parse = require('.')
+
+// TODO(anh): categorize these tests into their own test suite
+test('parse twice should be fine', async () => {
+    const xml = '<hello attr1="val1" attr2="val2">world</hello>'
+    expect(async () => {
+        await parse(xml)
+        await parse(xml)
+    }).not.toThrow()
+})
