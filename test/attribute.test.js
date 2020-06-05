c5f31a5fcbeebd77f4c078ce0b37d3ee13c4b95c
Tuan Anh Tran <me@tuananh.org>
2020-06-05 11:02:02 +0700

    test: moar test

--- test/attribute.test.js
+++ test/attribute.test.js
@@ -1,10 +1,18 @@
 const parse = require('.')
 
-test('attribute test', async () => {
+test('attribute test: simple element with text', async () => {
     const xml = '<hello attr1="val1" attr2="val2">world</hello>'
     expect(await parse(xml)).toEqual([
         ['startElement', 'hello', { attr1: 'val1', attr2: 'val2' }],
         ['text', 'world'],
         ['endElement', 'hello'],
+    ])
+})
+
+test('attribute test: element with self closing tag', async () => {
+    const xml = '<w:pStyle w:val="Hangingindent"/>'
+    expect(await parse(xml)).toEqual([
+        ['startElement', 'w:pStyle', { 'w:val': 'Hangingindent' }],
+        ['endElement', 'w:pStyle'],
     ])
 })
