374c839c7c0d2ea71ee96d3f0fe4ef238d24949b
Tuan Anh Tran <me@tuananh.org>
2020-06-09 07:01:01 +0700

    test: test issue in https://github.com/isaacs/sax-js/issues/246

---
+++ test/multiple-root-ele.test.js
@@ -0,0 +1,23 @@
+const parse = require('.')
+
+test('multiple root elements', async () => {
+    const xml = '<hello></hello><hello2></hello2>'
+    expect(await parse(xml)).toEqual([
+        ['startElement', 'hello', {}],
+        ['endElement', 'hello'],
+        ['startElement', 'hello2', {}],
+        ['endElement', 'hello2'],
+    ])
+})
+
+test('multiple root elements', async () => {
+    const xml = '<hello>world</hello><hello2>world2</hello2>'
+    expect(await parse(xml)).toEqual([
+        ['startElement', 'hello', {}],
+        ['text', 'world'],
+        ['endElement', 'hello'],
+        ['startElement', 'hello2', {}],
+        ['text', 'world2'],
+        ['endElement', 'hello2'],
+    ])
+})
