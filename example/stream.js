f9864adfb5920a4587fa2f0cd99a40ebf5338428
Tuan Anh Tran <me@tuananh.org>
2026-07-18 21:52:40 +0700

    update example stream
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- example/stream.js
+++ example/stream.js
@@ -3,7 +3,7 @@ const SaxParser = require('..')
 
 const parser = new SaxParser()
 
-const xml = '<hello>' + '<item id="1"><name>foo</name></item>'.repeat(100) + '</hello>'
+const xml = '<hello>' + '<item id="1"><name>foo</name></item>'.repeat(3) + '</hello>'
 const readStream = new Readable()
 readStream._read = () => {}
 
@@ -15,7 +15,13 @@ readStream.push(null)
 readStream
     .pipe(parser)
     .on('startElement', (name, attrs) => {
-        console.log('name', name)
+        console.log('startElement', name)
+    })
+    .on('endElement', (name, attrs) => {
+        console.log('endElement', name)
+    })
+    .on('startAttribute', attr => {
+        console.log('startAttribute', attr)
     })
     .on('text', (text) => {
         console.log('text', text)
